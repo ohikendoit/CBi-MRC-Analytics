1fe9393754e934dd5de974691c67c8d152739372
Kenneth Jung <ohikendoit@gmail.com>
2021-09-16 19:03:14 +0300

    SpaCy NER implementation

--- src/NER/spacy_model_pipeline.py
+++ src/NER/spacy_model_pipeline.py
@@ -18,6 +18,14 @@ while True:
     except OverflowError:
         maxInt = int(maxInt/10)
 
+def get_entities(x):
+    result = {}
+    doc = nlp(x)
+    for ent in doc.ents:
+        result[ent.label_] = ent.text
+    return result
+
+
 
 crisis = ['conflict', 'violence', 'displacement', 'drought', 'earthquake', 'fire', 'flooding', 'freeze', 'health emergency', 'dengue', 'pneumonic plague', 'measles', 'landslide', 'tropical storm', 'typhoon', 'cyclone', 'hurricane', 'tsunami', 'urban disaster', 'volcanic eruption', 'refugee', 'terrorist attack', 'cold wave', 'complex emergency', 'epidemic', 'extratropical cyclone', 'flash flood', 'flood', 'heat wave', 'insect infestation', 'land slide', 'mud slide', 'severe local storm', 'snow avalanche', 'storm surge', 'technological disaster', 'tropical cyclone', 'volcano', 'wild fire', 'flood crisis', 'victims', 'flood victims', 'flood powerful', 'powerful storms', 'hoisted flood', 'storms amazing', 'explosion', 'amazing rescue', 'rescue women', 'flood cost', 'counts flood', 'toll rises', 'braces river', 'river peaks', 'crisis deepens', 'prayers', 'thoughts prayers', 'affected tornado', 'affected', 'death toll', 'tornado relief', 'photos flood', 'water rises', 'toll', 'flood waters', 'flood appeal', 'victims explosion', 'bombing suspect', 'massive explosion', 'affected areas', 'praying victims', 'injured', 'please join', 'join praying', 'prayers people', 'redcross', 'text redcross', 'visiting flood', 'lurches fire', 'video explosion', 'deepens death', 'opposed flood', 'help flood', 'died explosions', 'marathon explosions', 'flood relief', 'donate', 'first responders', 'flood affected', 'donate cross', 'braces', 'tornado victims', 'deadly', 'prayers affected', 'explosions running', 'evacuated', 'relief', 'flood death', 'deaths confirmed', 'affected flooding', 'people killed', 'dozens', 'footage', 'survivor finds', 'worsens eastern', 'flood worsens', 'flood damage', 'people dead', 'girl died', 'donation help', 'major flood', 'rubble', 'another explosion', 'confirmed dead', 'rescue', 'send prayers', 'flood warnings', 'tornado survivor', 'damage', 'devastating', 'flood toll', 'affected hurricane', 'prayers families', 'releases photos', 'hundreds injured', 'inundated', 'crisis', 'text donation', 'redcross give', 'recede', 'bombing', 'massive', 'bombing victims', 'explosion ripped', 'gets donated', 'donated victims', 'relief efforts', 'news flood', 'flood emergency', 'give online', 'fire flood', 'huge explosion', 'bushfire', 'torrential rains', 'residents', 'breaking news', 'redcross donate', 'affected explosion', 'disaster', 'someone captured', 'tragedy', 'enforcement', 'people injured', 'twister', 'blast', 'injuries reported', 'fatalities', 'donated million', 'donations assist', 'dead explosion', 'survivor', 'death', 'suspect dead', 'peaks deaths', 'love prayers', 'explosion fertiliser', 'explosion reported', 'return home', 'evacuees', 'large explosion', 'firefighters', 'morning flood', 'praying', 'public safety', 'txting redcross', 'destroyed', 'displaced', 'fertilizer explosion', 'unknown number', 'donate tornado', 'retweet donate', 'flood tornado', 'casualties', 'climate change', 'financial donations', 'stay strong', 'dead hundreds', 'major explosion', 'bodies recovered', 'waters recede', 'response disasters', 'victims donate', 'unaccounted', 'fire fighters', 'explosion victims', 'prayers city', 'accepting financial', 'torrential', 'bomber', 'disasters txting', 'explosion registered', 'missing flood', 'volunteers', 'brought hurricane', 'relief fund', 'help tornado', 'explosion fire', 'ravaged', 'prayers tonight', 'tragic', 'enforcement official', 'saddened', 'dealing hurricane', 'impacted', 'flood recovery', 'stream', 'dead torrential', 'flood years', 'nursing', 'recover', 'responders', 'massive tornado', 'buried alive', 'alive rubble', 'crisis rises', 'flood peak', 'homes inundated', 'flood ravaged', 'explosion video', 'killed injured', 'killed people', 'people died', 'missing explosion', 'make donation', 'floods kill', 'tornado damage', 'entire crowd', 'cross tornado', 'terrifying', 'need terrifying', 'even scary', 'cost deaths', 'facing flood', 'deadly explosion', 'dead missing', 'floods force', 'flood disaster', 'tornado disaster', 'medical examiner', 'help victims', 'hundreds homes', 'severe flooding', 'shocking video', 'bombing witnesses', 'magnitude', 'firefighters police', 'fire explosion', 'storm', 'flood hits', 'floodwaters', 'emergency', 'flood alerts', 'crisis unfolds', 'daring rescue', 'tragic events', 'medical office', 'deadly tornado', 'people trapped', 'police officer', 'explosion voted', 'lives hurricane', 'bombings reports', 'breaking suspect', 'bombing investigation', 'praying affected', 'reels surging', 'surging floods', 'teenager floods', 'rescue teenager', 'appeal launched', 'explosion injured', 'injured explosion', 'responders killed', 'explosion caught', 'city tornado', 'help text', 'name hurricane', 'damaged hurricane', 'breaking arrest', 'suspect bombing', 'massive manhunt', 'releases images', 'shot killed', 'rains severely', 'house flood', 'live coverage', 'devastating tornado', 'lost lives', 'reportedly dead', 'following explosion', 'remember lives', 'tornado flood', 'want help', 'seconds bombing', 'reported dead', 'imminent', 'rebuild', 'safe hurricane', 'surviving', 'injuries', 'prayers victims', 'police suspect', 'warning', 'help affected', 'kills forces', 'dead floods', 'flood threat', 'military', 'flood situation', 'thousands homes', 'risk running', 'dead injured', 'dying hurricane', 'loss life', 'thoughts victims', 'bombing shot', 'breaking enforcement', 'police people', 'video capturing', 'feared dead', 'terrible explosion', 'prayers involved', 'reported injured', 'seismic', 'victims waters', 'flood homeowners', 'flood claims', 'homeowners reconnect', 'reconnect power', 'power supplies', 'rescuers help', 'free hotline', 'hotline help', 'please stay', 'investigation', 'saddened loss', 'identified suspect', 'bombings saddened', 'killed police', 'dead', 'praying community', 'registered magnitude', 'leave town', 'reported explosion', 'heart praying', 'life heart', 'prepare hurricane', 'landfall', 'crisis worsens', 'arrest', 'bombing case', 'suspect run', 'communities damaged', 'destruction', 'levy', 'tornado', 'hurricane coming', 'toxins flood', 'release toxins', 'toxins', 'supplies waters', 'crisis found', 'braces major', 'government negligent', 'attack', 'rebuilt communities', 'help rebuilt', 'rebuilt', 'rescuers', 'buried', 'heart prayers', 'flood levy', 'watch hurricane', 'victims lost', 'soldier', 'waiting hurricane', 'run massive', 'high river', 'terror', 'memorial service', 'terror attack', 'coast hurricane', 'terrified hurricane', 'aftermath', 'suspect killed', 'suspect pinned', 'lost legs', 'hurricane category', 'names terrified', 'authorities', 'assist people', 'hurricane black', 'unknown soldier', 'events', 'safety', 'troops', 'disaster relief', 'cleanup', 'troops lend', 'effected hurricane', 'time hurricane', 'saying hurricane', 'praying families', 'dramatic', 'path hurricane']
 crisis_event = ['Haiti Earthquake', 'Coronavirus', 'Coronavirus Disease', 'COVID-19', 'Typhoon Goni', 'Rolly', 'Typhoon Vamco', 'Ulysses', 'Beirut Port Explosions', 'Cyclone Harold', 'Harold', 'Hurricane Dorian', 'Dorian', 'Cyclone Kenneth', 'Kenneth', 'Cyclone Idai', 'Idai', 'Indonesia Tsunami', 'Typhoon Manghut', 'Manghut']
@@ -25,6 +33,7 @@ disaster_management = ['Caution and advice', 'Injured People', 'Dead people', 'I
 
 
 nlp = spacy.load("en_core_web_lg")
+nlp.max_length = 1500000
 ruler = EntityRuler(nlp, overwrite_ents=True)
 
 patterns_crisis = list(nlp.tokenizer.pipe(crisis))
@@ -32,7 +41,7 @@ patterns_crisis = [{"label": "CRISIS", "pattern": crisis[i], "id": crisis[i], "I
                    for i in range(len(crisis))]
 phrase_matcher_attr = "POS"
 ruler.add_patterns(patterns_crisis)
-nlp.add_pipe(ruler)
+#nlp.add_pipe(ruler)
 
 patterns_crisis_event = list(nlp.tokenizer.pipe(crisis_event))
 patterns_crisis_event = [{"label": "CRISIS_EVENT", "pattern": crisis_event[i], "id": crisis_event[i],
@@ -48,7 +57,10 @@ ruler.add_patterns(patterns_disaster_management)
 
 
 #doc = main_nlp('/src/df_extracted.csv')
-df_extracted = pd.read_csv('df_extracted.csv')
+df_extracted = pd.read_csv('df_extracted.csv', engine='python', encoding='utf-8', error_bad_lines=False)
+df_extracted = df_extracted[:1000]
+df_extracted['Matches'] = df_extracted['text'].apply(get_entities)
+
 
 #save a spaCy Model
-main_nlp.to_disk("disaster_management_ner")
+nlp.to_disk("disaster_management_ner")
